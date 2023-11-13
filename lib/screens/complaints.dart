4953493accfc3eaa32c075ede4e17512cc232cc7
kiranpranay <pranaybannu12@gmail.com>
2023-11-13 16:19:06 +0530

    askstm: update complaints screen
    Signed-off-by: kiranpranay <pranaybannu12@gmail.com>

--- lib/screens/complaints.dart
+++ lib/screens/complaints.dart
@@ -1,60 +1,207 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Complaints',
+      theme: ThemeData(),
+      home: const ComplaintsScreen(),
+    );
+  }
+}
 
 class ComplaintsScreen extends StatefulWidget {
-  const ComplaintsScreen({super.key});
+  const ComplaintsScreen({Key? key}) : super(key: key);
 
   @override
   _ComplaintsScreenState createState() => _ComplaintsScreenState();
 }
 
 class _ComplaintsScreenState extends State<ComplaintsScreen> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+  final TextEditingController complaintController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
-  final _complaintController = TextEditingController();
 
-  @override
-  void dispose() {
-    _complaintController.dispose();
-    super.dispose();
+  // Function to handle form submission
+  Future<void> _submitForm() async {
+    final formData = {
+      'name': nameController.text,
+      'email': emailController.text,
+      'phone': phoneController.text,
+      'complaint': complaintController.text,
+    };
+
+    final response = await http.post(
+      Uri.parse('https://apis.stmorg.in/complaints/add_complaints'),
+      body: formData,
+    );
+
+    if (response.statusCode == 200) {
+      // Handle successful submission
+      var data = jsonDecode(response.body);
+      if (data['status'] == 'success') {
+        var id = data['data'];
+        // display success message and clear form
+        nameController.clear();
+        emailController.clear();
+        phoneController.clear();
+        complaintController.clear();
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Complaint submitted with ID: $id'),
+          ),
+        );
+      } else {
+        print('Error submitting form. Status code: ${response.statusCode}');
+        print(data['data']);
+      }
+    } else {
+      // Handle error in submission
+      print('Error submitting form. Status code: ${response.statusCode}');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Complaints'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20, top: 50, right: 20),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              TextFormField(
-                controller: _complaintController,
-                decoration: const InputDecoration(
-                  labelText: 'Enter your complaint',
-                  border: OutlineInputBorder(),
-                ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Please enter your complaint';
-                  }
-                  return null;
-                },
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  // SvgPicture.asset("assets/icons/menu.svg"),
+                  // Go back to previous screen
+                  IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: const Icon(Icons.arrow_back),
+                    padding: const EdgeInsets.all(0),
+                    iconSize: 30,
+                  ),
+                  Image.asset("assets/images/logo.png", height: 35),
+                ],
               ),
-              const SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    // TODO: Submit complaint to server
-                    _complaintController.clear();
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('Complaint submitted')),
-                    );
-                  }
-                },
-                child: const Text('Submit'),
+              const SizedBox(height: 30),
+              const Text("Complaint Box",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+              const SizedBox(height: 10),
+              const Text(
+                "Alternatively, you can mail us at law@stmorg.in",
+                style: TextStyle(fontSize: 16),
+                textAlign: TextAlign.justify,
+              ),
+              const SizedBox(height: 30),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text("Complaint Form",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                ],
+              ),
+              const SizedBox(height: 20),
+              // Beautiful form
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      controller: nameController,
+                      decoration: const InputDecoration(
+                        labelText: "Name",
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter your name';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      controller: emailController,
+                      decoration: const InputDecoration(
+                        labelText: "Email",
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter your email';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      controller: phoneController,
+                      decoration: const InputDecoration(
+                        labelText: "Phone Number",
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter your phone number';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      controller: complaintController,
+                      maxLines: 5,
+                      decoration: const InputDecoration(
+                        labelText: "Complaint",
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter your complaint';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 20),
+
+                    // Submit button
+                    ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          _submitForm();
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 30, vertical: 15),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      child: const Text("Submit",
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white)),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
